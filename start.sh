fd26a1fe3c21169c5f04cf968f7d7df3f4045785
faridevnz <alessio.farinelli.developer@gmail.com>
2022-08-11 08:25:40 +0200

    setting node path into start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-echo "PATH --> ${PATH}"
+export PATH=$PATH:/root/.nvm/versions/node/v18.7.0/bin;
 
 PID=$(awk -F'LISTEN |/' '{print $2}' <<< "$(netstat -ltnp | egrep ':::3000')" | sed 's/\s//g')
 echo "detected pid running on :::3000 --> ${PID}"
@@ -10,3 +10,5 @@ fi
 
 echo "restart the wiki server"
 /var/www/wiki.faridevnz.me/node_modules/yarn/bin/yarn next > /var/www/wiki.faridevnz.me/logs/info.log 2> /var/www/wiki.faridevnz.me/logs/error.log &
+echo "PATH --> "
+echo $PATH
